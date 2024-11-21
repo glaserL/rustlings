11c6c95e99b5d7fc2795be6c697f9ef6b007cbcf
glaserL <33624744+glaserL@users.noreply.github.com>
2024-11-21 17:29:55 +0100

    Options

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,16 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        input
+            .iter()
+            .map(|(str, cmd)| match cmd {
+                Command::Uppercase => str.to_uppercase(),
+                Command::Trim => str.trim().to_owned(),
+                Command::Append(times) => format!("{str}{:}", "bar".repeat(*times)),
+            })
+            .collect()
+    }
 }
 
 fn main() {
@@ -36,8 +45,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
